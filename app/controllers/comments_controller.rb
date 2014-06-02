affdd46a339e18f3209e419b5c047eef8f80d58a
Dito <dhartoto@yahoo.com>
2014-06-02 16:29:45 +1000

    Updated to find by slug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_action :require_user, except: [:index, :show,]
 
   def create
-    @post = Post.find(params[:post_id])
+    @post = Post.find_by(slug: params[:post_id])
     @comment = Comment.new(set_comment)
     @comment.post = @post 
     @comment.creator = current_user
