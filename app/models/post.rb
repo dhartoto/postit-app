b41f1bb96a30a69ba38a1d2955c07e523ce3f8fa
Dito <dhartoto@yahoo.com>
2014-06-03 16:20:18 +1000

    Add module for votes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,34 +1,23 @@
 class Post < ActiveRecord::Base
+  include Voteable
+  
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User' 
   has_many :comments, dependent: :destroy
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
-  
-  before_save :create_slug
 
   validates :title, presence: true, length: {minimum: 5}
   validates :url, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :slug, uniqueness: true
-
-  def count_votes
-    count_true - count_false
-  end
-
-  def count_true
-    self.votes.where(vote: true).size
-  end
-
-  def count_false
-    self.votes.where(vote: false).size
-  end
   
   def create_slug
-    self.slug = self.title.gsub(' ', '_').downcase
+    slug = self.title.gsub(' ','_').downcase
+    slug = slug.strip
+    slug = slug.gsub(/\s*[^A-Za-z0-9]\s*/, '_')
+    slug = self.slug = slug.gsub(/-+/,'_')
     count = 1
-    while !!Post.exists?(slug: self.slug)
-      self.slug = self.slug + '_'+ count.to_s
+    while !!Post.exists?(slug: self.slug) && Post.find_by(slug: self.slug)!= self
+      self.slug = slug + '_'+ count.to_s
       count += 1
     end
     self.slug
---
+++ lib/voteable.rb
@@ -0,0 +1,20 @@
+module Voteable
+  extend ActiveSupport::Concern
+  
+  included do
+    has_many :votes, as: :voteable
+  end
+  
+  def count_votes
+    count_true - count_false
+  end
+
+  def count_true
+    self.votes.where(vote: true).size
+  end
+
+  def count_false
+    self.votes.where(vote: false).size
+  end
+  
+end
